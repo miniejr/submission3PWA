19d8065f8777474a7ba077447589aaf34f70b86c
miniejr <miniejr@gmail.com>
2020-08-16 22:08:57 +0700

    change link

--- js/script.js
+++ js/script.js
@@ -21,7 +21,7 @@ if (!("serviceWorker" in navigator)) {
 
 function registerServiceWorker() {
   return navigator.serviceWorker
-    .register("/workbox-sw.js")
+    .register("workbox-sw.js")
     .then((registration) => {
       console.log("Registrasi service worker berhasil.");
       return registration;
